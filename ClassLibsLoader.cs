ab4682fa7be4daf7c588a97c661332f3dc74097c
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-03-16 13:42:18 +0300

    Add base infrastructure

---
+++ ClassLibsLoader.cs
@@ -0,0 +1,30 @@
+﻿namespace MahoTrans.ClassLibs;
+
+/// <summary>
+///     Simple interface to classes hosted in MahoTrans.ClassLibs library.
+/// </summary>
+public static class ClassLibsLoader
+{
+    /// <summary>
+    ///     Gets full names of classes hosted here.
+    /// </summary>
+    public static IEnumerable<string> Names
+    {
+        get { return typeof(ClassLibsLoader).Assembly.GetManifestResourceNames().Where(x => x.EndsWith(".class")); }
+    }
+
+    /// <summary>
+    ///     Gets streams for classes hosted here. Do not forget to dispose them.
+    /// </summary>
+    public static List<Stream> GetClasses()
+    {
+        var list = new List<Stream>();
+        var ass = typeof(ClassLibsLoader).Assembly;
+        foreach (var name in Names)
+        {
+            list.Add(ass.GetManifestResourceStream(name)!);
+        }
+
+        return list;
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Helper dll for MT. Hosts binary libs that MT loads into itself.
 
 All classes not obfuscated (so MT can parse them). Use any decompiler to check their content directly.
 
-If not specified explicitly, classes are taken from [KEmulator nnmod](https://github.com/shinovon/KEmulator) or [J2MELoader](https://github.com/nikita36078/J2ME-Loader). Check their licenses/sources for further action.
+If not specified explicitly, classes are taken from [KEmulator nnmod](https://github.com/shinovon/KEmulator)
+or [J2MELoader](https://github.com/nikita36078/J2ME-Loader). Check git commit messages for each file to learn details. Check their licenses/sources for further action.
